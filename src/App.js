7ee3fdcd41d2eb3e54bef55a2c7e4ca4d9d8b2de
Victor Franco <vfranco.itc@gmail.com>
2021-08-15 16:23:03 -0500

    Added project files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tweet from './Tweet';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://coursera-twitter-api.herokuapp.com/tweets');
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+    
+  }
+
+  useEffect(() => {
+    console.log("here")
+    fetchData();
+  })
+
+  return (
+    <div style={{background: '#111111', padding: '30px'}}>
+       {data.map(tweet => <Tweet key ={tweet.id} {...tweet}/>)}
+    </div>
+  );
+}
+
+export default App;
